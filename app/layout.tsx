22eff246cd6af42d42194600c3a48bf154766c5c
avsipko <admin1@Sipko-test>
2024-11-10 20:06:11 +0300

    include fonts and logo

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,8 @@
 import '@/app/ui/global.css';
 
+import { inter } from '@/app/ui/fonts';
+
+
 export default function RootLayout({
   children,
 }: {
@@ -8,8 +11,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       
-      <body>
-        {children}</body>
+      <body className={`${inter.className} antialiased`}>{children}</body>
     </html>
   );
 }
